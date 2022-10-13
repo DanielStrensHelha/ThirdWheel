5bc93f107b94baf9e74f2a2714fbccf8f77b4a95
DanielStrensHelha <LA217024@student.helha.be>
2022-10-13 10:27:19 +0200

    Added sample code to create and draw lines and polygons on an html canvas

--- script/main.js
+++ script/main.js
@@ -1,4 +1,28 @@
-let gameBoard = document.getElementById("gameBoard");
-gameBoard.style = "width: 100px; \
-                height: 100px; \
-                background-color: red;";
+//Setting the main variables and constants
+const canvas = document.getElementById("gameCanvas");
+const context = canvas.getContext('2d');
+const debug = document.getElementById("debug");
+
+let windowHeight = window.innerHeight;
+let windowWidth = window.innerWidth;
+let d;
+d = (windowWidth <= windowHeight/2) ? windowWidth : windowHeight/2;
+let c = d/2, t = d/4, h = Math.sqrt((d/2)*(d/2) - (d/4)*(d/4));
+
+canvas.height = h*2;
+canvas.width = d;
+debug.innerText = "canvas side = " + d;
+
+//Test, drawing a line :
+context.moveTo(c, h);
+context.lineTo(c, 0);
+context.stroke();
+
+//Test, drawing a polygon in blue :
+context.beginPath();
+context.moveTo(10,10);
+context.lineTo(40, 40);
+context.lineTo(100, 40);
+context.lineTo(70, 10);
+context.lineTo(10, 10);
+context.fill();
